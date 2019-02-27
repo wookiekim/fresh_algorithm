acd7f1f7e66c0a2a68a3d78af5ecbdb6c6704333
Chan Young Kim <shinychan95@postech.ac.kr>
2019-02-27 18:41:48 +0900

    6549 수정, 3777 정답, 5620 임시

---
+++ algorithms/divide_and_conquer/shinychan95/5620/5620.py
@@ -0,0 +1,23 @@
+""" Problem 5629 """
+
+
+
+
+
+
+def main():
+    points = [(1, 1), (1, 5), (2, 3), (2, 4), (3, 0), (3, 6),
+              (4, 2), (4, 4), (5, 4), (6, 2), (6, 6)]
+    count = len(points)
+
+
+    pass
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        main()
